60d84d07a31a80f9379dffbd610496a47a54a3cf
tony <chenszhao@ebay.com>
2020-07-23 17:38:28 +0800

    new feature to upgrade project

--- Miffy.java
+++ Miffy.java
@@ -2,6 +2,7 @@ package com.miffyq.studygit
 
 public class Miffy {
     public static void main(String[] args) {
-	System.out.println("This is miffy-q");
+	     System.out.println("This is miffy-q");
+       System.out.println("And I want to search my GitHub.");
     }
 }
